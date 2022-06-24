9bbbd9564bd9b7eb5da38d3b2c22154e1b29bfaf
Jeppe Frandsen <jeppe@algrens.dk>
2022-06-24 16:31:39 +0200

    Corrected setting of output

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,14 @@ GITHUB_REF_NAME=$2
 if [[ $GITHUB_REF_NAME == */merge ]]; then
   RELEASE_CHANGES=0
   echo "Workflow is a pull request. Setting changes to false"
+  echo ::set-output name=changes::false
 else
   RELEASE_LATEST=$(git tag --sort committerdate --merged "$GITHUB_REF_NAME" | tail -1)
   RELEASE_CHANGES=$(git log --pretty=%h "$RELEASE_LATEST..$GITHUB_SHA" | wc -l)
-  echo "Found $RELEASE_CHANGES changes for commit $GITHUB_SHA on $GITHUB_REF_NAME"
+  echo "Found $RELEASE_CHANGES change(s) for commit $GITHUB_SHA on $GITHUB_REF_NAME"
+  if [[ $RELEASE_CHANGES -gt 0 ]]; then
+    echo ::set-output name=changes::true
+  else
+    echo ::set-output name=changes::false
+  fi
 fi
-
-echo ::set-output name=changes::$((RELEASE_CHANGES > 0))
