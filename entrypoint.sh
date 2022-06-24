531c3c04f22f979e055b1638a8adcf49e993187a
Jeppe Frandsen <jeppe@algrens.dk>
2022-06-24 16:22:50 +0200

    Added better logging for pull requests

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,14 @@ set -e
 
 GITHUB_SHA=$1
 GITHUB_REF_NAME=$2
-RELEASE_CHANGES=0
 
-if [[ $GITHUB_REF_NAME != */merge ]]; then
+if [[ $GITHUB_REF_NAME == */merge ]]; then
+  RELEASE_CHANGES=0
+  echo "Workflow is a pull request. Setting changes to false"
+else
   RELEASE_LATEST=$(git tag --sort committerdate --merged "$GITHUB_REF_NAME" | tail -1)
   RELEASE_CHANGES=$(git log --pretty=%h "$RELEASE_LATEST..$GITHUB_SHA" | wc -l)
+  echo "Found $RELEASE_CHANGES changes for commit $GITHUB_SHA on $GITHUB_REF_NAME"
 fi
 
-echo "Found $RELEASE_CHANGES changes for commit $GITHUB_SHA on $GITHUB_REF_NAME "
 echo ::set-output name=changes::$((RELEASE_CHANGES > 0))
