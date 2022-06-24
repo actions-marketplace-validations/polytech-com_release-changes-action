d1a68a8b0a4679c740984cbb574b48c7eb2f0ce5
Jeppe Frandsen <jeppe@algrens.dk>
2022-06-24 16:14:29 +0200

    Added check for pull request

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,12 @@ set -e
 
 GITHUB_SHA=$1
 GITHUB_REF_NAME=$2
+RELEASE_CHANGES=0
 
-echo "GITHUB_REF_NAME = $GITHUB_REF_NAME"
+if [[ $GITHUB_REF_NAME != */merge ]]; then
+  RELEASE_LATEST=$(git tag --sort committerdate --merged "$GITHUB_REF_NAME" | tail -1)
+  RELEASE_CHANGES=$(git log --pretty=%h "$RELEASE_LATEST..$GITHUB_SHA" | wc -l)
+fi
 
-RELEASE_LATEST=$(git tag --sort committerdate --merged "$GITHUB_REF_NAME" | tail -1)
-RELEASE_CHANGES=$(git log --pretty=%h "$RELEASE_LATEST..$GITHUB_SHA" | wc -l)
-
+echo "Found $RELEASE_CHANGES changes for commit $GITHUB_SHA on $GITHUB_REF_NAME "
 echo ::set-output name=changes::$((RELEASE_CHANGES > 0))
