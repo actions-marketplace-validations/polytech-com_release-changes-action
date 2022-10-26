ca4634fe5baaa87128970ecd29590a66e96d5f23
Jeppe Frandsen <jeppe@algrens.dk>
2022-10-26 21:14:55 +0200

    Updated to new syntax

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,16 +6,16 @@ GITHUB_REF_NAME=$2
 
 if [[ $GITHUB_REF_NAME == */merge ]]; then
   echo -e "\U0001f680 Pull request detected. Setting changes to false"
-  echo ::set-output name=changes::false
+  echo "changes=false" >> $GITHUB_OUTPUT
 else
   RELEASE_LATEST=$(git tag --sort committerdate --merged "$GITHUB_REF_NAME" | tail -1)
   RELEASE_CHANGES=$(git log --pretty=%h "$RELEASE_LATEST..$GITHUB_SHA" | wc -l)
   echo -e "\U0001f680 Found $RELEASE_CHANGES change(s) for commit $GITHUB_SHA on $GITHUB_REF_NAME"
   if [[ $RELEASE_CHANGES -gt 0 ]]; then
-    echo ::set-output name=changes::true
+    echo "changes=true" >> $GITHUB_OUTPUT
   else
-    echo ::set-output name=changes::false
+    echo "changes=false" >> $GITHUB_OUTPUT
   fi
 fi
 
-echo ::set-output name=latest::"$RELEASE_LATEST"
+echo "latest=$RELEASE_LATEST" >> $GITHUB_OUTPUT
